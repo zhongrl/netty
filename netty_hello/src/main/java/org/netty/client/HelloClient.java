ff12c189d2943e79cfe5ea376e6f6e288c522719
zhongrulei <zhongrulei@xiaoniu66.com>
2016-02-19 11:39:17 +0800

    netty
    netty

---
+++ netty_hello/src/main/java/org/netty/client/HelloClient.java
@@ -0,0 +1,62 @@
+package org.netty.client;
+
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.SimpleChannelHandler;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+
+
+public class HelloClient {
+
+	private static int num = 1;
+	
+	 public static void main(String args[]) {  
+		 for (int i = 0; i < 100; i++) {
+			 nettyNumber();
+			 num ++ ;
+		}
+	 }  
+	 
+	 public static void nettyNumber(){
+		 System.out.println("===========调用服务开始==============" );
+	        // Client服务启动器  
+	        ClientBootstrap bootstrap = new ClientBootstrap(  
+	                new NioClientSocketChannelFactory(  
+	                        Executors.newCachedThreadPool(),  
+	                        Executors.newCachedThreadPool()));  
+	        // 设置一个处理服务端消息和各种消息事件的类(Handler)  
+	        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {  
+	            @Override  
+	            public ChannelPipeline getPipeline() throws Exception {  
+	                return Channels.pipeline(new HelloClientHandler());  
+	            }  
+	        });  
+	        // 连接到本地的8000端口的服务端  
+	        bootstrap.connect(new InetSocketAddress(  
+	                "127.0.0.1", 8000));  
+	        System.out.println("===========调用服务结束==============");
+	 }
+	  
+	    private static class HelloClientHandler extends SimpleChannelHandler {  
+	  
+	  
+	        /** 
+	         * 当绑定到服务端的时候触发，打印"Hello world, I'm client." 
+	         *  
+	         * @alia OneCoder 
+	         * @author lihzh 
+	         */  
+	        @Override  
+	        public void channelConnected(ChannelHandlerContext ctx,  
+	                ChannelStateEvent e) {  
+	            System.out.println("Hello world, I'm client.请求次数 :" + num);  
+	        }  
+	    }  
+}
